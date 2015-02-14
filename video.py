c54348484fdd66a798c601c3627162ce59136f2d
Alex Patel <alexanderpatel@college.harvard.edu>
2015-02-14 09:40:27 -0500

    get video metadata from KA API

--- video.py
+++ video.py
@@ -1,3 +1,5 @@
+import json
+import urllib2 
 import urlparse
 
 class Video:
@@ -14,3 +16,38 @@ class Video:
         path = parsed.path
         subpaths = path.split('/')
         self.readable_id = subpaths[len(subpaths) - 1]
+
+    def query_video_data(self):
+        """ Query video metadata from Khan Academy API. """
+        readable_id = self.readable_id 
+        api_base = "http://www.khanacademy.org/api/v1/videos/{0}" 
+        api_query = api_base.format(self.readable_id)
+        request = urllib2.Request(api_query)
+        try:
+            response = urllib2.urlopen(request)
+            data = json.loads(response.read())
+            self.raw_json = data
+        except urllib2.URLError, AttributeError:
+            Exception("Unable to access Khan Academy API: ", api_query)
+
+    def extract_json_data(self):
+        """ Extract video metadata from raw API response. """
+        raw_json = self.raw_json
+        self.description = raw_json['description']
+        self.title = raw_json['title']
+        self.duration = raw_json['duration']
+        self.download_urls = raw_json['download_urls'] 
+        if self.download_urls['mp4']:
+            self.mp4 = self.download_urls['mp4']
+            self.set_video_path()
+        else:
+            raise Exception("No MP4 available for video.")
+
+    def get_metadata(self):
+        """
+        Find and store appropriate video metadata.
+        """
+        print "Getting video metadata..."
+        self.extract_readable_id()
+        self.query_video_data() 
+        self.extract_json_data()

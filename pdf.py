461705041f8cc2480dadcaabcc2a4eb4466af52f
Alex Patel <alexanderpatel@college.harvard.edu>
2015-02-14 14:27:56 -0500

    pdf init and cursor movement

--- pdf.py
+++ pdf.py
@@ -12,3 +12,30 @@ margin_vert = settings.margin_vert
 font = settings.font
 fontSize = settings.fontSize
 
+cursor = 0
+
+def pdf_init(video):
+    """ Initialize and return pdf canvas for video. """
+    fn = video.readable_id + ".pdf"
+    c = canvas.Canvas(fn, pagesize=letter)
+    set_font(c)
+    reset_cursor(c)
+    return c
+
+def set_font(canvas):
+    canvas.setFont(font, fontSize, leading = fontSize)
+
+def reset_cursor(canvas):
+    """ Reset canvas cursor to top left of page. """
+    global cursor
+
+    _, height = letter
+    canvas.translate(margin_horiz, height - margin_vert)
+    cursor = 0
+
+def new_page(canvas):
+    """ Move canvas to next page and reset cursor. """
+    global cursor 
+    canvas.showPage()
+    set_font(canvas)
+    reset_cursor(canvas)

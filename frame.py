3c65284ab8eb3256b8652679afbb27f309662ef5
Alex Patel <alexanderpatel@college.harvard.edu>
2015-02-14 09:58:41 -0500

    frame worker thread to serve video frames by index

---
+++ frame.py
@@ -0,0 +1,50 @@
+import copy
+import threading
+
+import cv2
+
+index_slot = None
+frame_slot = None
+
+cv = threading.Condition()
+
+def frame_worker(video):
+    """ Thread to open video and serve frames by index. """
+    global index_slot
+    global frame_slot
+    global cv
+
+    cv.acquire()
+    capture = cv2.VideoCapture(video.video_path)
+
+    # Save frame information and notify waiting main() thread.
+    video.width = int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)) 
+    video.height = int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)) 
+    video.nframes = int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+    cv.notify()
+
+    # Accept requests from get_frame() indefinitely.
+    while True:
+        # Wait from queries from get_frame instance.
+        cv.wait()
+        index = index_slot
+        index_slot = None 
+        assert(index < video.nframes)
+        if index == -1:
+            # Exit loop and return -1 upon get_frame(-1).
+            frame_slot = -1 
+            cv.notify()
+            break
+        # Set pointer to next frame to index.
+        capture.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, index)
+        success, frame = capture.read()
+        if not success:
+            raise Exception("Failed to read frame {0}".format(index))
+        # Send frame to get_frame() call.
+        assert(frame_slot is None)
+        frame_slot = frame
+        cv.notify()
+
+    # Close video and return.
+    capture.release() 
+    cv.release()
